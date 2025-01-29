609be8190c5122a589ef2d7977a3d79c8475d155
Gabriel <115955863+gabsjf@users.noreply.github.com>
2025-01-29 13:02:44 -0400

    fix path img abrir_chamado.php

--- abrir_chamado.php
+++ abrir_chamado.php
@@ -24,7 +24,7 @@
 
     <nav class="navbar navbar-dark bg-dark">
       <a class="navbar-brand" href="#">
-        <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        <img src="/public/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
         Chamados' Help
       </a>
       <ul class ="nav-nav">
