11861c35e36034196fdb9fc385aa759373c4014f
Gabriel <115955863+gabsjf@users.noreply.github.com>
2025-01-29 12:47:21 -0400

    Update registra_chamado.php

--- registra_chamado.php
+++ registra_chamado.php
@@ -4,7 +4,7 @@ $titulo = str_replace('#','-',$_POST['titulo']);
 $categoria = str_replace('#','-',$_POST['categoria']);
 $descricao = str_replace('#','-',$_POST['descricao']);
 $texto =$_SESSION['id']. '#'. $titulo .'#'. $categoria .'#'. $descricao . PHP_EOL;
-$arquivo = fopen('../../app_help_desk/arquivo.txt','a');
+$arquivo = fopen('includes/valida_login.php','a');
 fwrite($arquivo, $texto);
 fclose($arquivo);
 
